a73a58ef7140d9d9958bab8d69e2aae21712d747
time2fishman <adam_h_martinez@hotmail.com>
2022-09-22 12:04:47 -0600

    changing some state on button click

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Adam",
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+          <p>Hi {this.state.name}</p>
+          <button
+            onClick={() => {
+              this.setState({ name: "Andre" });
+            }}
           >
-            Learn React
-          </a>
+            Change Name
+          </button>
         </header>
       </div>
     );
